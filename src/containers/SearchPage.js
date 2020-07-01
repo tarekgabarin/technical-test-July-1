c3935018f234759cf63f183231b5fa921b5e13b5
Tarek Gabarin <tarekgabarin1992@gmail.com>
2020-07-01 17:34:30 -0400

    added validation

--- src/containers/SearchPage.js
+++ src/containers/SearchPage.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Card from '../components/Card';
-
+import { Formik } from 'formik';
+import * as Yup from 'yup'
 
 export default function SearchPage() {
     return (
@@ -11,40 +12,73 @@ export default function SearchPage() {
                 </div>
 
                 <Card>
-                
-                    <form>
-                        <div className="card-body">
+                    <Formik
+                        initialValues={{ searchCityText: '', searchNameText: '', searchAddressText: '' }}
+                        validateSchema={Yup.object().shape({
+                            searchCityText: Yup.string()
+                                .required('Field required'),
+                        })}
+                        onSubmit={(values, { setSubmitting }) => {
+                            setTimeout(() => {
+                                alert(JSON.stringify(values, null, 2));
+                                setSubmitting(false);
+                            }, 400);
+                        }}
+                    >
 
-                        <div className="form-group">
-                        <label className="form-label" htmlFor="searchCity">City</label>
-                        <input className="form-input" type="text" id="searchCity" placeholder="Toronto" />
-                        </div>
+                        {({
+                            values,
+                            handleChange,
+                            handleBlur,
+                            handleSubmit,
+                           /* isSubmitting, */
+                            /* and other goodies */
+                        }) => (
+                                <form onSubmit={handleSubmit}>
 
-                        <div className="form-group">
-                        <label className="form-label" htmlFor="searchName">Name</label>
-                        <input className="form-input" type="text" id="searchName" placeholder="Bob's Burgers" />
-                        </div>
+                                    <div className="card-body">
 
-                        <div className="form-group">
-                        <label className="form-label" htmlFor="searchAddress">Address</label>
-                        <input className="form-input" type="text" id="searchAddress" placeholder="123 street" />
-                        </div>
+                                    <div className="form-group">
+                                                <label className="form-label" htmlFor="searchCity">City</label>
+                                                <input className="form-input" onChange={handleChange}
+                                                    onBlur={handleBlur} name={'searchCityText'}   type="text" value={values.searchCityText}  id="searchCity" placeholder="Toronto" />
+                                    </div>
 
-                        <div className="form-group">
+                                        
 
-                            
+                                        <div className="form-group">
+                                            <label className="form-label" htmlFor="searchName">Name</label>
+                                            <input className="form-input" onChange={handleChange}
+                                                onBlur={handleBlur} name={'searchNameText'} value={values.searchNameText} type="text" id="searchName" placeholder="Bob's Burgers" />
+                                        </div>
 
-                        <button className='is-button is-full-width is-button-primary'>
-                            Search
-                        </button>
+                                        <div className="form-group">
+                                            <label className="form-label" htmlFor="searchAddress">Address</label>
+                                            <input className="form-input" onChange={handleChange}
+                                                onBlur={handleBlur} name={'searchAddressText'} value={values.searchAddressText} type="text" id="searchAddress" placeholder="123 street" />
+                                        </div>
 
-                        </div>
+                                        <div className="form-group">
 
-                        </div>
-                        
-                    </form>
+                                            {(values.searchCityText.trim() !== "") ? (
+                                            <button className='is-button is-full-width is-button-primary'>
+                                                Search
+                                             </button>
+                                            ) : (
+                                            <button className='is-button is-full-width is-button-primary' disabled>
+                                                Search
+                                             </button>
+                                            )}
+
+                                            
+
+                                        </div>
+
+                                    </div>
+                                </form>
+                            )}
+                    </Formik>
                 </Card>
-
             </div>
         </div>
     )
