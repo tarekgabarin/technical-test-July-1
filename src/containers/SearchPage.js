6690b1ddd25194011bf749d765ee204f77d33902
Tarek Gabarin <tarekgabarin1992@gmail.com>
2020-07-01 15:09:28 -0400

    did basic styling for seach page

--- src/containers/SearchPage.js
+++ src/containers/SearchPage.js
@@ -1,18 +1,48 @@
 import React from 'react';
 import Card from '../components/Card';
-import CardHeader from '../components/CardHeader';
+
 
 export default function SearchPage() {
     return (
         <div className='container'>
             <div className="flex-column space-between">
                 <div>
-                    <h1>Open Table</h1>
+                    <h1>Search Restraurants</h1>
                 </div>
 
                 <Card>
+                
+                    <form>
+                        <div className="card-body">
 
-                    <CardHeader title={'Search Restaurants'} />
+                        <div className="form-group">
+                        <label className="form-label" htmlFor="searchCity">City</label>
+                        <input className="form-input" type="text" id="searchCity" placeholder="Toronto" />
+                        </div>
+
+                        <div className="form-group">
+                        <label className="form-label" htmlFor="searchName">Name</label>
+                        <input className="form-input" type="text" id="searchName" placeholder="Bob's Burgers" />
+                        </div>
+
+                        <div className="form-group">
+                        <label className="form-label" htmlFor="searchAddress">Address</label>
+                        <input className="form-input" type="text" id="searchAddress" placeholder="123 street" />
+                        </div>
+
+                        <div className="form-group">
+
+                            
+
+                        <button className='is-button is-full-width is-button-primary'>
+                            Search
+                        </button>
+
+                        </div>
+
+                        </div>
+                        
+                    </form>
                 </Card>
 
             </div>
